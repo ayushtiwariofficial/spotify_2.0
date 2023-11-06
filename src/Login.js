d77a02c7d4be8f763de26110e106c43aad067d58
Ayush <ayushtiwari615@gmail.com>
2023-11-07 00:24:57 +0530

    basic setup of project and creating login page

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './login.css'
+
+function login() {
+  return (
+    <div className='login'>
+      <img src='https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg'
+      alt='spotify' />
+      <a href='#'>Login with Spotify</a>
+    </div>
+  )
+}
+
+export default login
