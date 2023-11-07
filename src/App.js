d0b35c735f16663e5fe453dae7a81402ec0696cb
Ayush <ayushtiwari615@gmail.com>
2023-11-08 00:58:35 +0530

    added spotify.js and configured spotify api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Login from './Login';
+import React, {useEffect, useState} from 'react';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi;
 
 function App() {
+  const [token, setToken] = useState();
+  useEffect(() =>{
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token ;
+
+    if(_token){
+      setToken(_token);
+      spotify.setAccessToken(_token);
+    }
+    console.log("Token",token)
+  },[])
+
   return (
     <div className="App">
-      <Login />
+      {token ? <h1>Logged in</h1> : <Login/>}
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './login.css'
+import { loginUrl } from './spotify'
 
 function login() {
   return (
     <div className='login'>
-      <img src='https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg'
+      <img src='banner.png'
       alt='spotify' />
-      <a href='#'>Login with Spotify</a>
+      <a href={loginUrl}>Login with Spotify</a>
     </div>
   )
 }
