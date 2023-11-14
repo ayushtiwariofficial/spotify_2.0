07aa5cd179e8ba4189f927aee00b5e30c012ddcb
Ayush <ayushtiwari615@gmail.com>
2023-11-15 01:11:31 +0530

    added sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Sidebar.css"
+import SidebarOption from './SidebarOption'
+import { Home } from '@material-ui/icons'
+import { Search } from '@material-ui/icons'
+import { LibraryMusic } from '@material-ui/icons'
+import { useDataLayerValue } from './DataLayer'
+
+
+function Sidebar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
